a935138ba49d613490dd4623927dcd3b2a88f6b5
Jeff Morales <jeff.morales@outlook.com>
2023-04-23 07:25:23 -0500

    Updated code for line 52

--- data/process_data.py
+++ data/process_data.py
@@ -49,7 +49,7 @@ def clean_data(df):
 def save_data(df, database_filename):
     '''save dataframe as sql database file'''
     engine = create_engine('sqlite:///' + database_filename)
-    df.to_sql('DisasterResponseDatabase', engine, index=False)  
+    df.to_sql('DisasterResponseDatabase', engine, index=False, if_exists='replace')  
 
 
 def main():
